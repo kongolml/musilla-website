0f73691ac737bf65e0f4a1f4963a292f2f81282b
kongolml <kongol.ml@gmail.com>
2014-06-22 21:05:02 +0300

    now slider works

--- js/played-songs-slider.js
+++ js/played-songs-slider.js
@@ -1,13 +1,31 @@
 $(document).ready(function (){
+
+	//set width of the #played-songs-container
+	var songItems = 0;
+	var songItemsWidth = $(".song-item").outerWidth(true);
+	var playedSongsContainerWidth = 0;
+
+	$(".song-item").each(function() {
+		songItems++;
+	});
+
+	playedSongsContainerWidth = songItemsWidth * songItems;
+
+	$("#played-songs-container").css("width", playedSongsContainerWidth);
+
+
+	//slide to the left
 	$("button.left").click(function(){
 		var leftCoord = parseInt($("#played-songs-container").css("left"));
-		leftCoord = leftCoord - 165;
+		leftCoord = leftCoord - 170;
 		$("#played-songs-container").css("left", leftCoord+"px");
 	});
 
+
+	//slide to the right
 	$("button.right").click(function(){
 		var leftCoord = parseInt($("#played-songs-container").css("left"));
-		leftCoord = leftCoord + 165;
+		leftCoord = leftCoord + 170;
 		$("#played-songs-container").css("left", leftCoord+"px");
 	});
 });

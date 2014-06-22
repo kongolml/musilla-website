470482e9c7e0db49fece8c0710953b6a4a628005
kongolml <kongol.ml@gmail.com>
2014-06-23 00:02:12 +0300

    some addings

--- js/played-songs-slider.js
+++ js/played-songs-slider.js
@@ -17,15 +17,44 @@ $(document).ready(function (){
 	//slide to the left
 	$("button.left").click(function(){
 		var leftCoord = parseInt($("#played-songs-container").css("left"));
-		leftCoord = leftCoord - 170;
+		leftCoord = leftCoord + 170;
 		$("#played-songs-container").css("left", leftCoord+"px");
+
+		//disable slide button, if it's end of the slider
+		var sliderPosition = parseInt($("#played-songs-container").css("left"));
+		if (sliderPosition >= 45) {
+			$("button.left").addClass("disabled");
+			$("button.left").prop("disabled", true);
+		} else {
+			$("button.left").removeClass("disabled");
+			$("button.left").prop("disabled", false);
+			$("button.right").removeClass("disabled");
+			$("button.right").prop("disabled", false);
+		}
 	});
 
 
 	//slide to the right
 	$("button.right").click(function(){
 		var leftCoord = parseInt($("#played-songs-container").css("left"));
-		leftCoord = leftCoord + 170;
+		leftCoord = leftCoord - 170;
 		$("#played-songs-container").css("left", leftCoord+"px");
+
+		//disable slide button, if it's end of the slider
+		var sliderPosition = parseInt($("#played-songs-container").css("left"));
+		var sliderEnd = parseInt(sliderPosition + $("#played-songs-container").width());
+		if (sliderEnd < 900) {
+			$("button.right").addClass("disabled");
+			$("button.right").prop("disabled", true);
+		} else {
+			$("button.right").removeClass("disabled");
+			$("button.right").prop("disabled", false);
+		}
+
+		if (sliderPosition < 45) {
+			$("button.left").removeClass("disabled");
+			$("button.left").prop("disabled", false);
+		}
 	});
+
 });
